f9d3706b6978f5b23cde7d55bfe5406f32181687
Tom Depplito <tomdepplito@gmail.com>
2013-07-11 16:21:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfferPadmapper::Application.config.secret_token = '73c3fe5d6224917d8c190d1f2cf0d1b02ac40fdd192224727665f245eefa4704102489f053446c71b715f4c9b2d5d0c1d6befa0f126a02dec76eff550c683359'
