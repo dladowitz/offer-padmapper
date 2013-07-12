1d8a30aaa70c4906606196a7cc704a729f220708
David Ladowitz <david@ladowitz.com>
2013-07-12 14:39:04 -0700

    agularized some shit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,20 +5,20 @@ var city1 = new google.maps.LatLng(37.850033,-110.4183);
 var city2 = new google.maps.LatLng(37.850033,-100.4183);
 var us_center = new google.maps.LatLng(37.09024, -95.712891);
 
-var offer1 = {name: "Battle Cats Video", reward_amount: 10, currency: "Stars", dollar_value: 50, latitude: 37.850033, longitude: -120.4183}
-var offer2 = {name: "Dorritos Commercial ", reward_amount: 20, currency: "Gold", dollar_value: 10, latitude: 40.850033, longitude: -90.4183}
-var offer3 = {name: "American Express", reward_amount: 5, currency: "Diamonds", dollar_value: 450, latitude: 35.850033, longitude: -85.4183}
-var offer4 = {name: "Ford Focus", reward_amount: 30, currency: "Start", dollar_value: 150, latitude: 42.850033, longitude: -71.4183}
-var offer5 = {name: "CitiCard", reward_amount: 8, currency: "Cat Food", dollar_value: 250, latitude: 31.850033, longitude: -110.4183}
-var offer6 = {name: "Clash of the clans", reward_amount: 10, currency: "Suns", dollar_value: 90, latitude: 40.850033, longitude: -110.4183}
-var offer7 = {name: "Ninja Warroirs", reward_amount: 9, currency: "Silver", dollar_value: 10, latitude: 37.850033, longitude: -100.4183}
-var offer8 = {name: "Candy Crush Video", reward_amount: 10, currency: "Cat Food", dollar_value: 20, latitude: 33.850033, longitude: -80.4183}
-var offer9 = {name: "Virgin Atlantic", reward_amount: 30, currency: "Rings", dollar_value: 350, latitude: 34.850033, longitude: -90.4183}
-var offer10 = {name: "Battle Cats Video 2", reward_amount: 10, currency: "Mice", dollar_value: 540, latitude: 32.850033, longitude: -100.4183}
-var offer11 = {name: "McDonald's", reward_amount: 40, currency: "Cat Food", dollar_value: 150, latitude: 31.850033, longitude: -85.4183}
-var offer12 = {name: "Hay Day Video", reward_amount: 9, currency: "Dollars", dollar_value: 220, latitude: 45.850033, longitude: -100.4183}
+// var offer1 = {name: "Battle Cats Video", reward_amount: 10, currency: "Stars", dollar_value: 50, latitude: 37.850033, longitude: -120.4183}
+// var offer2 = {name: "Dorritos Commercial ", reward_amount: 20, currency: "Gold", dollar_value: 10, latitude: 40.850033, longitude: -90.4183}
+// var offer3 = {name: "American Express", reward_amount: 5, currency: "Diamonds", dollar_value: 450, latitude: 35.850033, longitude: -85.4183}
+// var offer4 = {name: "Ford Focus", reward_amount: 30, currency: "Start", dollar_value: 150, latitude: 42.850033, longitude: -71.4183}
+// var offer5 = {name: "CitiCard", reward_amount: 8, currency: "Cat Food", dollar_value: 250, latitude: 31.850033, longitude: -110.4183}
+// var offer6 = {name: "Clash of the clans", reward_amount: 10, currency: "Suns", dollar_value: 90, latitude: 40.850033, longitude: -110.4183}
+// var offer7 = {name: "Ninja Warroirs", reward_amount: 9, currency: "Silver", dollar_value: 10, latitude: 37.850033, longitude: -100.4183}
+// var offer8 = {name: "Candy Crush Video", reward_amount: 10, currency: "Cat Food", dollar_value: 20, latitude: 33.850033, longitude: -80.4183}
+// var offer9 = {name: "Virgin Atlantic", reward_amount: 30, currency: "Rings", dollar_value: 350, latitude: 34.850033, longitude: -90.4183}
+// var offer10 = {name: "Battle Cats Video 2", reward_amount: 10, currency: "Mice", dollar_value: 540, latitude: 32.850033, longitude: -100.4183}
+// var offer11 = {name: "McDonald's", reward_amount: 40, currency: "Cat Food", dollar_value: 150, latitude: 31.850033, longitude: -85.4183}
+// var offer12 = {name: "Hay Day Video", reward_amount: 9, currency: "Dollars", dollar_value: 220, latitude: 45.850033, longitude: -100.4183}
 
-var offers = [offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12,offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12,offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12,offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12]
+// var offers = [offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12,offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12,offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12,offer1, offer2, offer3, offer4, offer5, offer6, offer7, offer8, offer9, offer10, offer11, offer12]
 
 var markers = [];
 var infoWindows =[];
@@ -31,7 +31,7 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
 
@@ -93,35 +93,3 @@ window.onload = function ()
 {
   drop();
 }
-=======
-var offerPad = angular.module('offerPad', []);
-
-offerPad.factory('pusher', function($rootScope) {
-  var pusher = new Pusher('265c8065bb95051d0a67');
-  var channel = pusher.subscribe('long_lat_stream');
-
-  Pusher.log = function(message) {
-    if(window.console && window.console.log) {
-      window.console.log(message);
-    }
-  };
-
-  return {
-    bind: function(event, callback) {
-      channel.bind(event, function() {
-        var args = arguments;
-        $rootScope.$apply(function() {
-          callback.apply(channel, args);
-        });
-      });
-    }
-  };
-});
-
-offerPad.controller('MapCtrl', [$scope, pusher, function($scope, pusher) {
-  var $scope.locations = [];
-  pusher.bind('new_location', function(data) {
-    $scope.messages.push(data);
-  });
-}]);
->>>>>>> 0bce124... Setup angular and pusher
